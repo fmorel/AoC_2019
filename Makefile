1d2a19c795ffdad7bb0a8dd691921cfd2bd49aa6
FX Morel <efix.morel@gmail.com>
2020-03-04 15:52:56 +0100

    Day 9 complete ;)

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-all: day_2 day_3 day_4 day_5 day_6 day_7 day_8
+all: day_2 day_3 day_4 day_5_9 day_6 day_7 day_8
 
 CFLAGS+= -g
 
 day_2: day_2.o intcode.o
-day_5: day_5.o intcode.o
+day_5_9: day_5_9.o intcode.o
 day_7: day_7.o intcode.o
 
--- day_5_9.c
+++ day_5_9.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #include "intcode.h"
 
-#define PROGRAM_SIZE 768
+#define PROGRAM_SIZE 2048
 
 int main(int argc, char **argv)
 {
@@ -14,6 +15,7 @@ int main(int argc, char **argv)
     Context ctx;
 
     input[0] = atoi(argv[2]);
+    memset(program, 0, sizeof(program));
 
     ctx.input = input;
     ctx.input_idx = 0;
@@ -25,7 +27,7 @@ int main(int argc, char **argv)
     run(&ctx);
 
     for (i = 0; i < ctx.output_idx; i++) {
-        printf("Out[%d] : %d\n", i, ctx.output[i]);
+        printf("Out[%d] : %ld\n", i, ctx.output[i]);
     }
 
     return 0;
