4e1e4adc4dbd36d901db8c257733d3d61e8aedbd
François-Xavier Morel <fx.morel@amarisoft.com>
2020-03-13 17:39:40 +0100

    Day 11: step 1

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-all: day_2 day_3 day_4 day_5_9 day_6 day_7 day_8 day_10
+all: day_2 day_3 day_4 day_5_9 day_6 day_7 day_8 day_10 day_11
 
 CFLAGS+= -g -lm
 
 day_2: day_2.o intcode.o
 day_5_9: day_5_9.o intcode.o
 day_7: day_7.o intcode.o
+day_11: day_11.o intcode.o
 
---
+++ day_11.c
@@ -0,0 +1,123 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "intcode.h"
+
+#define PROGRAM_SIZE 640
+#define GRID_SIZE 256
+#define INPUT_SIZE 32
+
+#define PAINT_BIT (1 << 7)
+uint8_t grid[GRID_SIZE * GRID_SIZE];
+
+enum {
+    ROBOT_UP,
+    ROBOT_RIGHT,
+    ROBOT_DOWN,
+    ROBOT_LEFT
+};
+
+typedef struct {
+    int x;
+    int y;
+    int dir;
+    int cur_prog_pos;
+    int n_painted_once;
+    Context brain;
+} Robot;
+
+void init_grid(void)
+{
+    memset(grid, 0, sizeof(grid));
+}
+
+void init_robot(Robot *r, int64_t *program)
+{
+    r->x = 0;
+    r->y = 0;
+    r->cur_prog_pos = 0;
+    r->dir = ROBOT_UP;
+    r->n_painted_once = 0;
+    r->brain.program = program;
+    r->brain.input = malloc(INPUT_SIZE * sizeof(int64_t));
+    r->brain.output = malloc(2*sizeof(int64_t));
+}
+
+void step_robot(Robot *r)
+{
+    switch (r->dir) {
+        case ROBOT_UP:
+            (r->y)--;
+            break;
+        case ROBOT_RIGHT:
+            (r->x)++;
+            break;
+        case ROBOT_DOWN:
+            (r->y)++;
+            break;
+        case ROBOT_LEFT:
+            (r->x)--;
+            break;
+    }
+    if (abs(r->x) >= GRID_SIZE/2 || abs(r->y) >= GRID_SIZE/2) {
+        printf("Robot out of the grid (%d,%d)\n", r->x, r->y);
+        exit(-1);
+    }
+    printf("%d : (%d,%d)\n", r->dir, r->x, r->y);
+}
+
+void run_robot(Robot *r)
+{
+    int x, y, i;
+    for(;;) {
+        x = GRID_SIZE/2 + r->x;
+        y = GRID_SIZE/2 + r->y;
+
+        r->brain.input_idx = 0;
+        r->brain.output_idx = 0;
+        /* The robot can use several input instructions even for a single step */
+        for (i = 0; i < INPUT_SIZE; i++) {
+            r->brain.input[i] = (grid[y * GRID_SIZE + x] & 0x7F);
+        }
+        
+        /* Let the robot produce two outputs */
+        r->cur_prog_pos = resume_till_output(&r->brain, r->cur_prog_pos);
+        if (r->cur_prog_pos < 0)
+            break;
+        r->cur_prog_pos = resume_till_output(&r->brain, r->cur_prog_pos);
+       
+       /* Output 1 : color */
+        if ((grid[y * GRID_SIZE + x] & PAINT_BIT) == 0)
+            r->n_painted_once++;
+        
+        grid[y * GRID_SIZE + x] = PAINT_BIT | (r->brain.output[0] & 0x7F);
+
+        /* Output 2 : rotation then move */
+        r->dir = (4 + r->dir + (r->brain.output[1]*2 - 1)) % 4;
+        step_robot(r);
+    }
+}
+
+
+
+int main(int argc, char **argv)
+{
+    int64_t program[PROGRAM_SIZE];
+    Robot robot;
+    int size;
+    
+    size = parse_program(argv[1], program);
+    if (size > PROGRAM_SIZE) {
+        printf("Program too big %d\n", size);
+        return -1;
+    }
+
+    init_grid();
+    init_robot(&robot, program);
+    run_robot(&robot);
+    printf("Number of panels painted once %d\n", robot.n_painted_once);
+
+    return 0;
+}
